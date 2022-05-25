cb5438c1c5e7c9db1a0a57399b47becaec9999df
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-05-25 22:21:09 +0100

    Add Render

--- dist/index.ad9a7e97.js
+++ dist/index.ad9a7e97.js
@@ -8,41 +8,123 @@ const React = {
                 children
             }
         };
-        console.log(element);
         return element;
     }
 };
-const App = ()=>/*#__PURE__*/ React.createElement("div", {
+let states = [];
+let stateCursor = 0;
+const useState = (initialState)=>{
+    const CURSOR = stateCursor;
+    states[CURSOR] = states[CURSOR] || initialState;
+    const setState = (newState)=>{
+        console.log(states);
+        states[CURSOR] = newState;
+        rerender();
+    };
+    stateCursor++;
+    return [
+        states[CURSOR],
+        setState
+    ];
+};
+const App = ()=>{
+    const [name, setName] = useState('React');
+    const [count, setCount] = useState(0);
+    return /*#__PURE__*/ React.createElement("div", {
         className: "container",
         __source: {
             fileName: "index.tsx",
-            lineNumber: 14,
-            columnNumber: 20
+            lineNumber: 30,
+            columnNumber: 13
         },
         __self: this
     }, /*#__PURE__*/ React.createElement("h1", {
         __source: {
             fileName: "index.tsx",
-            lineNumber: 15,
+            lineNumber: 31,
             columnNumber: 9
         },
         __self: this
-    }, "Hello React"), /*#__PURE__*/ React.createElement("p", {
+    }, "Hello ", name), /*#__PURE__*/ React.createElement("input", {
+        value: name,
+        placeholder: "Name",
+        onchange: (e)=>setName(e.target.value)
+        ,
         __source: {
             fileName: "index.tsx",
-            lineNumber: 16,
+            lineNumber: 32,
             columnNumber: 9
         },
         __self: this
-    }, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus delectus dignissimos dolorem dolores eum, mollitia nulla praesentium similique sint veritatis? Animi architecto aut harum libero necessitatibus quae, quam saepe temporibus?"))
-;
-/*#__PURE__*/ React.createElement(App, {
+    }), /*#__PURE__*/ React.createElement("h2", {
+        __source: {
+            fileName: "index.tsx",
+            lineNumber: 34,
+            columnNumber: 9
+        },
+        __self: this
+    }, count), /*#__PURE__*/ React.createElement("button", {
+        onclick: ()=>setCount(count + 1)
+        ,
+        __source: {
+            fileName: "index.tsx",
+            lineNumber: 35,
+            columnNumber: 9
+        },
+        __self: this
+    }, "+"), /*#__PURE__*/ React.createElement("button", {
+        onclick: ()=>setCount(count - 1)
+        ,
+        __source: {
+            fileName: "index.tsx",
+            lineNumber: 36,
+            columnNumber: 9
+        },
+        __self: this
+    }, "-"), /*#__PURE__*/ React.createElement("p", {
+        __source: {
+            fileName: "index.tsx",
+            lineNumber: 37,
+            columnNumber: 9
+        },
+        __self: this
+    }, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus delectus dignissimos dolorem dolores eum, mollitia nulla praesentium similique sint veritatis? Animi architecto aut harum libero necessitatibus quae, quam saepe temporibus?"));
+};
+const render = (reactElement, container)=>{
+    if ([
+        "string",
+        "number"
+    ].includes(typeof reactElement)) {
+        container.appendChild(document.createTextNode(reactElement));
+        return;
+    }
+    const actualDomElement = document.createElement(reactElement.tag);
+    if (reactElement.props) Object.keys(reactElement.props).filter((c)=>c !== 'children'
+    ).forEach((p)=>actualDomElement[p] = reactElement.props[p]
+    );
+    if (reactElement.props.children) reactElement.props.children.forEach((c)=>render(c, actualDomElement)
+    );
+    container.appendChild(actualDomElement);
+};
+const rerender = ()=>{
+    document.querySelector('#root').firstChild.remove();
+    stateCursor = 0;
+    render(/*#__PURE__*/ React.createElement(App, {
+        __source: {
+            fileName: "index.tsx",
+            lineNumber: 68,
+            columnNumber: 12
+        },
+        __self: this
+    }), document.querySelector('#root'));
+};
+render(/*#__PURE__*/ React.createElement(App, {
     __source: {
         fileName: "index.tsx",
-        lineNumber: 26,
-        columnNumber: 1
+        lineNumber: 72,
+        columnNumber: 8
     },
     __self: this
-});
+}), document.querySelector('#root'));
 
 //# sourceMappingURL=index.ad9a7e97.js.map
--- index.tsx
+++ index.tsx
@@ -5,13 +5,14 @@ const React = {
             return tag(props)
         }
         const element = {tag, props: {...props, children}}
-        console.log(element)
         return element
     }
-
 }
 
-const App = () => (<div className={"container"}>
+
+const App = () => {
+
+    return (<div className={"container"}>
         <h1>Hello React</h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -20,7 +21,26 @@ const App = () => (<div className={"container"}>
             architecto aut harum libero necessitatibus quae,
             quam saepe temporibus?
         </p>
-    </div>
-    );
+    </div>)
+};
 
-<App />
+
+const render = (reactElement, container) => {
+    if (["string", "number"].includes(typeof reactElement)) {
+        container.appendChild(document.createTextNode(reactElement))
+        return
+    }
+    const actualDomElement = document.createElement(reactElement.tag)
+    if (reactElement.props) {
+        Object.keys(reactElement.props).filter(c => c !== 'children').forEach(p =>
+            actualDomElement[p] = reactElement.props[p]
+        )
+    }
+    if (reactElement.props.children) {
+        reactElement.props.children.forEach(c => render(c, actualDomElement))
+    }
+    container.appendChild(actualDomElement)
+}
+
+render(<App/>, document.querySelector('#root'))
+
